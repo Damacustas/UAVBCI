2581e0317b0384248862b00c8006d4297cc4da39
Lars Bokkers <l.bokkers@student.ru.nl>
2015-01-26 16:41:32 +0100

    Initial import. Started work on joystick input.

---
+++ UAV/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAV.Joystick;
+
+namespace UAV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var js = new Joystick.Joystick();
+            js.Initialize("/dev/input/js0");
+            js.ReadInput();
+        }
+    }
+}
