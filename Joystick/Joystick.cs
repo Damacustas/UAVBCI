27295ebbd1758dec4c24616204c3962b40bfd597
Lars Bokkers <l.bokkers@student.ru.nl>
2015-01-26 20:36:24 +0100

    Seperated Joystick.cs contents into multiple files, added AR.Drone submodule, prepared flight control application for joystick.

--- Joystick/Joystick.cs
+++ Joystick/Joystick.cs
@@ -15,46 +15,58 @@ namespace UAV.Joystick
 
         private bool running = false;
 		private string deviceFile = null;
+        private Thread joystickInputThread = null;
 
         private ConcurrentQueue<JoystickEventArgs> changes = new ConcurrentQueue<JoystickEventArgs>();
 
+        /// <summary>
+        /// Intializes the joystick instance to respond to the given device file.
+        /// </summary>
+        /// <param name="deviceFile">The file that represents the joystick.</param>
         public void Initialize(string deviceFile)
 		{
 			this.deviceFile = deviceFile;
 			running = true;
-			new Thread(InputThread).Start();
+            joystickInputThread = new Thread(JoystickInputThread);
+            joystickInputThread.Start();
 		}
 
-        public void ProcessChanges()
+        /// <summary>
+        /// Closes the joystick device file and stops the internal input thread.
+        /// </summary>
+        /// <param name="clearPendingEvents">If <c>true</c>, clears the pending events not yet processed.</param>
+        /// <param name="clearState">If <c>true</c>, clears the joystick state.</param>
+        public void Deinitialize(bool clearPendingEvents = true, bool clearState = true)
+        {
+            deviceFile = null;
+            running = false;
+            joystickInputThread.Join();
+            
+            if(clearPendingEvents)
+            {
+                changes = new ConcurrentQueue<JoystickEventArgs>();
+            }
+            
+            if(clearState)
+            {
+                AxisValues.Clear();
+                ButtonStates.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Processes events received from the joystick I/O thread.
+        /// </summary>
+        public void ProcessEvents()
         {
             JoystickEventArgs e = null;
 			while (!changes.IsEmpty)
             {
                 if(changes.TryDequeue(out e))
                 {
-                    if(e.IsButtonEvent)
-                    {
-						if (ButtonStates.ContainsKey(e.Button))
-						{
-							ButtonStates[e.Button] = e.IsPressed;
-						}
-						else
-						{
-							ButtonStates.Add(e.Button, e.IsPressed);
-						}
-                    }
-                    else
-                    {
-						if (AxisValues.ContainsKey(e.Axis))
-						{
-							AxisValues[e.Axis] = e.Value;
-						}
-						else
-						{
-							AxisValues.Add(e.Axis, e.Value);
-						}
-                    }
+                    UpdateState(e);
 
+                    // If there are subscriptions to the event, raise the event.
                     if (InputReceived != null)
                     {
                         InputReceived(this, e);
@@ -63,10 +75,44 @@ namespace UAV.Joystick
             }
         }
 
-        private void InputThread()
+        /// <summary>
+        /// Updates the Joystick state by applying the given event.
+        /// </summary>
+        /// <param name="e">The event to apply to the state.</param>
+        private void UpdateState(JoystickEventArgs e)
+        {
+            if (e.IsButtonEvent)
+            {
+                if (ButtonStates.ContainsKey(e.Button))
+                {
+                    ButtonStates[e.Button] = e.IsPressed;
+                }
+                else
+                {
+                    ButtonStates.Add(e.Button, e.IsPressed);
+                }
+            }
+            else
+            {
+                if (AxisValues.ContainsKey(e.Axis))
+                {
+                    AxisValues[e.Axis] = e.Value;
+                }
+                else
+                {
+                    AxisValues.Add(e.Axis, e.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads from the joystick input stream and enques events to the internal event queue.
+        /// </summary>
+        private void JoystickInputThread()
         {
 			using(FileStream inputStream = new FileStream(deviceFile, FileMode.Open))
 			{
+                // Read and process in blocks of 8 bytes.
 	            byte[] buff = new byte[8];
 	            while (running)
 	            {
@@ -75,14 +121,19 @@ namespace UAV.Joystick
 	                if(joystickEvent != null)
 	                {
 	                    changes.Enqueue(joystickEvent);
-						//InputReceived(this, joystickEvent);
 	                }
 				}
 			}
         }
 
+        /// <summary>
+        /// Decodes input to an event.
+        /// </summary>
+        /// <param name="buff">The buffer to decode.</param>
+        /// <returns>The decoded event.</returns>
         private JoystickEventArgs DecodeJoystickEvent(byte[] buff)
         {
+            // Configuration is there to indicate what inputs there are on the joystick.
             if(checkBit(buff[6], (byte)MODE.CONFIGURATION))
             {
                 if(checkBit(buff[6], (byte)TYPE.AXIS))
@@ -98,7 +149,7 @@ namespace UAV.Joystick
                     return null;
                 }
             }
-            else
+            else // Everything else is a value.
             {
                 if (checkBit(buff[6], (byte)TYPE.AXIS))
                 {
@@ -119,10 +170,23 @@ namespace UAV.Joystick
             }
         }
 
-        enum STATE : byte { PRESSED = 0x01, RELEASED = 0x00 }
-        enum TYPE : byte { AXIS = 0x02, BUTTON = 0x01 }
-        enum MODE : byte { CONFIGURATION = 0x80, VALUE = 0x00 }
+        /// <summary>
+        /// The state of a button.
+        /// </summary>
+        private enum STATE : byte { PRESSED = 0x01, RELEASED = 0x00 }
 
+        // The type of input.
+        private enum TYPE : byte { AXIS = 0x02, BUTTON = 0x01 }
+
+        // The mode of the buffer input.
+        private enum MODE : byte { CONFIGURATION = 0x80, VALUE = 0x00 }
+
+        /// <summary>
+        /// Helper function to check if value has bit flag set to 1.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <param name="flag">The flag to check in value.</param>
+        /// <returns><c>True</c> if flag was set if value, <c>false</c> if not.</returns>
         bool checkBit(byte value, byte flag)
         {
             byte c = (byte)(value & flag);
@@ -130,165 +194,4 @@ namespace UAV.Joystick
         }
     }
 
-    public delegate void JoystickInputDelegate(object sender, JoystickEventArgs e);
-
-    public class JoystickEventArgs : EventArgs
-    {
-        public bool IsButtonEvent { get; internal set; }
-
-        public float Value { get; internal set; }
-        public byte Axis { get; internal set; }
-
-        public bool IsPressed { get; internal set; }
-        public byte Button { get; internal set; }
-    }
-    
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Checks if device parameter is defined properly.
-            string deviceFile = "";
-            if (args.Length == 2 && args[0] == "-d")
-            {
-                deviceFile = args[1];
-            }
-            else
-            {
-                Console.WriteLine("Wrong parameters. Example: ' -d /dev/input/js0'");
-                return;
-            }
-
-            // Device file from parameter.
-            Console.Clear();
-            Console.WriteLine(string.Format("Device file: {0}", deviceFile));
-
-            // Check if it exist.
-            if (!File.Exists(deviceFile))
-            {
-                Console.WriteLine("Does not exists !");
-                return;
-            }
-
-            // Read loop.
-            using (FileStream fs = new FileStream(deviceFile, FileMode.Open))
-            {
-                byte[] buff = new byte[8];
-                Joystick j = new Joystick();
-
-                while (true)
-                {
-                    // Read 8 bytes from file and analyze.
-                    fs.Read(buff, 0, 8);
-                    j.DetectChange(buff);
-
-                    int top = 1;
-
-                    // Prints Axis values
-                    foreach (byte key in j.Axis.Keys)
-                    {
-                        writeLine(top, string.Format("Axis{0}: {1}", key, j.Axis[key]));
-                        top += 1;
-                    }
-
-                    // Prints Buttons values
-                    foreach (byte key in j.Button.Keys)
-                    {
-                        writeLine(top, string.Format("Button{0}: {1}", key, j.Button[key]));
-                        top += 1;
-                    }
-                }
-            }
-        }
-
-        static void writeLine(int top, string value)
-        {
-            if (top < Console.BufferHeight)
-            {
-                Console.SetCursorPosition(0, top);
-                Console.WriteLine(value.PadRight(Console.BufferWidth, ' '));
-            }
-        }
-
-        public class Joystick
-        {
-            public Joystick()
-            {
-                Button = new Dictionary<byte, bool>();
-                Axis = new Dictionary<byte, short>();
-            }
-
-            enum STATE : byte { PRESSED = 0x01, RELEASED = 0x00 }
-            enum TYPE : byte { AXIS = 0x02, BUTTON = 0x01 }
-            enum MODE : byte { CONFIGURATION = 0x80, VALUE = 0x00 }
-
-            /// <summary>
-            /// Buttons collection, key: address, bool: value
-            /// </summary>
-            public Dictionary<byte, bool> Button;
-
-            /// <summary>
-            /// Axis collection, key: address, short: value
-            /// </summary>
-            public Dictionary<byte, short> Axis;
-
-            /// <summary>
-            /// Function recognizes flags in buffer and modifies value of button, axis or configuration.
-            /// Every new buffer changes only one value of one button/axis. Joystick object have to remember all previous values.
-            /// </summary>
-            public void DetectChange(byte[] buff)
-            {
-                // If configuration
-                if (checkBit(buff[6], (byte)MODE.CONFIGURATION))
-                {
-                    if (checkBit(buff[6], (byte)TYPE.AXIS))
-                    {
-                        // Axis configuration, read address and register axis
-                        byte key = (byte)buff[7];
-                        if (!Axis.ContainsKey(key))
-                        {
-                            Axis.Add(key, 0);
-                            return;
-                        }
-                    }
-                    else if (checkBit(buff[6], (byte)TYPE.BUTTON))
-                    {
-                        // Button configuration, read address and register button
-                        byte key = (byte)buff[7];
-                        if (!Button.ContainsKey(key))
-                        {
-                            Button.Add((byte)buff[7], false);
-                            return;
-                        }
-                    }
-                }
-
-                // If new button/axis value
-                if (checkBit(buff[6], (byte)TYPE.AXIS))
-                {
-                    // Axis value, decode U2 and save to Axis dictionary.
-                    short value = BitConverter.ToInt16(new byte[2] { buff[4], buff[5] }, 0);
-                    Axis[(byte)buff[7]] = value;
-                    return;
-                }
-                else if (checkBit(buff[6], (byte)TYPE.BUTTON))
-                {
-                    // Bytton value, decode value and save to Button dictionary.
-                    Button[(byte)buff[7]] = buff[4] == (byte)STATE.PRESSED;
-                    return;
-                }
-            }
-
-            /// <summary>
-            /// Checks if bits that are set in flag are set in value.
-            /// </summary>
-            bool checkBit(byte value, byte flag)
-            {
-                byte c = (byte)(value & flag);
-                return c == (byte)flag;
-            }
-        }
-    }
-
-    
 }
--- UAV/Program.cs
+++ UAV/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AR.Drone.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UAV.Joystick;
 
@@ -9,24 +11,70 @@ namespace UAV
 {
     class Program
     {
+        static DroneClient client = new DroneClient();
+
         static void Main(string[] args)
         {
             var js = new Joystick.Joystick();
             js.Initialize("/dev/input/js0");
             js.InputReceived += Js_InputReceived;
 
+            Thread.Sleep(500); // 500ms should be enough to open the file and read config.
+
+            if(js.AxisValues[2] != 0)
+            {
+                Console.WriteLine("gaz control != 0, not starting!");
+                return;
+            }
+
+            client.Start();
+
             while (true)
             {
-				js.ProcessChanges();
+				js.ProcessEvents();
             }
         }
 
         private static void Js_InputReceived(object sender, JoystickEventArgs e)
         {
-			if (!e.IsButtonEvent && e.Axis == 1)
-			{
-				Console.WriteLine("Axis {0}: {1}", 1, e.Value);
-			}
+            if (e.IsButtonEvent)
+            {
+                if (e.Button == 0 && e.IsPressed)
+                {
+                    client.Hover();
+                }
+                else if (e.Button == 1 && e.IsPressed)
+                {
+                    client.Emergency();
+                }
+                else if(e.Button == 2 && e.IsPressed)
+                {
+                    client.Takeoff();
+                }
+                else if(e.Button == 3 && e.IsPressed)
+                {
+                    client.Land();
+                }
+            }
+            else
+            {
+                if (e.Axis == 0)
+                {
+                    client.Progress(AR.Drone.Client.Command.FlightMode.Progressive, roll: 0.05f * e.Value);
+                }
+                else if (e.Axis == 1)
+                {
+                    client.Progress(AR.Drone.Client.Command.FlightMode.Progressive, pitch: 0.05f * e.Value);
+                }
+                else if(e.Axis == 2)
+                {
+                    client.Progress(AR.Drone.Client.Command.FlightMode.Progressive, gaz: 0.25f * e.Value);
+                }
+                else if(e.Axis == 3)
+                {
+                    client.Progress(AR.Drone.Client.Command.FlightMode.Progressive, yaw: 0.05f * e.Value);
+                }
+            }
         }
     }
 }
