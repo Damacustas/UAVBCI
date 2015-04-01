deb88f311d0eef96d40e1344f81d349ab395c4dd
Lars Bokkers <l.bokkers@student.ru.nl>
2015-04-01 21:17:03 +0200

    Created simulation project.

---
+++ UAV.Simulation/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UAV.Simulation
+{
+	public class Location
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public Location (double x = 0, double y = 0)
+		{
+		}
+
+		public double Distance(Location other)
+		{
+			return Math.Sqrt((oth
+		}
+	}
+}
+
---
+++ UAV.Simulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UAV.Simulation
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
